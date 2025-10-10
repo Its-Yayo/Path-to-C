b88687fa5279596514db2c84e6133678468542b0
Its-Yayo <yayitzz@proton.me>
2025-10-10 06:26:37 -0600

    files update #2

--- 2-variables/files.c
+++ 2-variables/files.c
@@ -8,9 +8,11 @@ int main(void) {
    *
    * putchar(args) -> escribe el caracter cada vez que se invoca  
    *
+   * getchar() -> lee solo un caracter, a diferencia de scanf() que 
+   * lee algo en la entrada (stdin)
    *
-   *
-   */
+   * putchar(args) -> solo inserta lo que getchar() leyo
+   */ 
 
   long c; // entero largo
 
@@ -19,6 +21,8 @@ int main(void) {
   // EOF -> End of File
   // Cada vez que iteremos, le sumaremos un valor a C hasta llegar a 
   // EOF
+  // read() -> si hay EOF
+  // pero en este caso no hay
   while (getchar() != EOF) {
     // Escribe el caracter y vuelve a leer el siguiente sucesivamente
     c++; // c = c + 1 || c =+ 1
