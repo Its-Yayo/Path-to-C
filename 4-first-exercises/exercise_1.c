97a0cad68f6e83b27f5ce86da5952c757d90bce5
Its-Yayo <yayitzz@proton.me>
2025-10-11 23:08:01 -0600

    exercises #1

--- 4-first-exercises/exercise_1.c
+++ 4-first-exercises/exercise_1.c
@@ -1,9 +1,25 @@
 /*
- * Dado un positivo N, se imprime sucesivamente su valor
- * 
- *
- *
- * in -> 
- * out ->
- *
- * */
+Dado un positivo N, se imprime sucesivamente su valor (multiplicado por 2) hasta que el resultado sea mayor o igual a 0
+El valor limite se debe declarar como constante
+
+ 
+in -> 4 
+out -> 8 16 32 64 128
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAXIMO 100
+
+
+int main(void) {
+  int x; // stdin
+  scanf("%i", &x); // pide valor de entrada
+  
+  while (x <= MAXIMO) {
+    x *= 2;
+    printf("%i ", x);
+  }
+}
