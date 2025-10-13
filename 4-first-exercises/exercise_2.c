2e8e18b7a4e128cfa790677b7413af53dd8f30fe
Its-Yayo <yayitzz@proton.me>
2025-10-12 20:17:46 -0600

    exercises #2 v1

--- 4-first-exercises/exercise_2.c
+++ 4-first-exercises/exercise_2.c
@@ -2,7 +2,8 @@
 Ejercicio 2: Contar vocales en una línea
 Planteamiento del problema:
 
-Se debe leer una cadena ingresada por el usuario carácter por carácter hasta encontrar un salto de línea (\n). Luego, se debe contar cuántas vocales (a, e, i, o, u, sin importar mayúsculas o minúsculas) contiene.
+Se debe leer una cadena ingresada por el usuario carácter por carácter hasta encontrar un salto de línea (\n). Luego, se debe contar cuántas 
+vocales (a, e, i, o, u, sin importar mayúsculas o minúsculas) contiene.
 
 Entrada:
 
@@ -17,6 +18,39 @@ donde X es el número de vocales encontradas.
 Ejemplo:
 
 in -> Hola Mundo  
-out -> Número de vocales: 4
+out -> 4
 
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+static char letters[] = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'}; 
+
+
+int main(void) {
+  char str[] = "";
+  scanf("%[^\n]%*c", &str); // Regex que toma en cuenta los whitespaces
+                            // hasta terminar el stdin
+  
+  // debug
+  printf("%s\n", str);
+
+
+  int cont = 0;
+
+  // FIXME
+  for (int i = 0; i < strlen(str); i++) {
+    for (int j = 0; j < (sizeof(letters) / sizeof(letters[0])); j++) {
+      if (str[i] == letters[j]) {
+        cont += 1;
+      }
+    } 
+  }
+
+  printf("%i", cont);
+}
+
+
