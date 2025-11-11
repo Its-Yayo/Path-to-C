6c9687d340a5fe2e7c84e142e1ad3c156ee92172
Its-Yayo <yayitzz@proton.me>
2025-11-11 07:08:13 -0600

    changes

--- 3-arrays/example2.c
+++ 3-arrays/example2.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int moving_arrays(int[][] *array) {
+  // TODO here
+}
+
 int main(void) {
   int myArray[] = {4, 5, 6, 7};
   int size = sizeof(myArray) / sizeof(myArray[0]);
