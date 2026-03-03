12d246ef1a5427062e57bdfd044691024e1e9837
Its-Yayo <yayitzz@proton.me>
2026-03-03 17:47:02 -0600

    check #2 update lock macros

---
+++ 01-C-Programming-A-Modern-Approach/1-Introduction/main.c
@@ -0,0 +1,35 @@
+/*
+ * Function to compute a single program that deploys how many bills bank has to deliver
+ * 
+ * Ej:
+ * 55 bills 
+ * 55 / 20 = 2 (res -> 55 - 40 = 15)
+ * 15 / 10 = 1 (res -> 15 - 10 = 5)
+ * 5 / 5 = 1 (res -> 5 - 5 = 0)
+ * end of loop
+ * Hooyeah */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+// arg -> amount of money
+// returns -> string with details of how many bills bank needs to deliver
+int
+bills(int amount) {
+	int amount_1 = amount / 20; // n bills of 20
+	// TODO -> manana sin falta lo pongo
+	int amount_2 = ()
+}
+
+int
+main(void) {
+	printf("%d\n", bills(55));
+}
+
+
+
+
+
+
