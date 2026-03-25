233f2540b593e575f4c299b2d8252483d66070e4
Its-Yayo <yayitzz@proton.me>
2026-03-24 21:08:02 -0500

    staged #2

--- 000-Exercises/01-minishell.c
+++ 000-Exercises/01-minishell.c
@@ -20,8 +20,10 @@ main(void) {
   while (1) {
     printf("> ");
     fgets(input, sizeof(input), stdin);
-
-    printf("You typed %s", input);
+    printf("You typed %s", input); 
+    
+    // "\n" cleaned for \0 that fgets(3) generates
+    input[strcspn(input, "\n")] = '\0';
 
     if (strcmp(input, "exit") == 0) {
       break;
